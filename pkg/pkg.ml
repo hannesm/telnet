e777603af6d9a28987e8ad94c7a8bc31ef3fbe42
Hannes Mehnert <hannes@mehnert.org>
2016-09-30 07:42:17 +0100

    topkg and travis

---
+++ pkg/pkg.ml
@@ -0,0 +1,17 @@
+#!/usr/bin/env ocaml
+#use "topfind"
+#require "topkg"
+open Topkg
+
+let mirage = Conf.with_pkg ~default:false "mirage"
+
+let () =
+  let opams =
+    [ Pkg.opam_file "opam" ~lint_deps_excluding:(Some ["ppx_tools"; "io-page"; "ipaddr"]) ]
+  in
+  Pkg.describe ~opams "otr" @@ fun c ->
+  let mirage = Conf.value c mirage in
+  Ok [
+    Pkg.mllib ~api:["Telnet"] "src/telnet.mllib";
+    Pkg.mllib ~cond:mirage "mirage/telnet_mirage.mllib" ~dst_dir:"mirage/"
+  ]
